80581d43d0ac033485cbd16e29613b3a720f530d
thamerh <hamdithamer99@gmail.com>
2022-09-13 17:13:41 +0100

    feat: add model for chat data base document

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,1 +1,26 @@
 import mongoose from "mongoose";
+
+const chatModel= mongoose.Schema({
+    chatName:{ type: String ,trim:true},
+    isGroupeChat:{ type:Boolean ,default:false},
+    user:[
+        {
+          type:  mongoose.Schema.Types.ObjectId,
+          ref: "user",
+
+        },
+    ],
+    latesMessages:{
+        type:  mongoose.Schema.Types.ObjectId,
+        ref:"Message",
+    },
+    groupAdmin:{
+        type:  mongoose.Schema.Types.ObjectId,
+        ref:"user",
+    }
+},
+{
+    timestamps: true,
+});
+const chat= mongoose.model("chat",chatModel);
+export default chat;
