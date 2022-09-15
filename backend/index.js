fd329764c092db35b970bd97e9f216498f3ce050
thamerh <hamdithamer99@gmail.com>
2022-09-15 12:24:30 +0100

    feat: integreate router in index.js server file

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import DBconnect from "./config/db.js"
+import router from "./routes/routes.js";
 
 const app=express();
 dotenv.config();
@@ -9,6 +10,8 @@ DBconnect();
 app.get("/",(req,res)=>{
 res.send("API IS RUNNING")
 });
+app.use(express.json());
+app.use(router);
 app.use(cors({ credentials:true, origin:process.env.URL}));
 const port=process.env.PORT || 5000
 app.listen(port,console.log(`server started in port ${port}`));
