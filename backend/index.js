0669baac69defdb1427c2fbbf07bb922f1abd25f
thamerh <hamdithamer99@gmail.com>
2022-09-15 10:18:06 +0100

    feat: integrate data base connection in  server file

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import DBconnect from "./config/db.js"
 
 const app=express();
 dotenv.config();
-
+DBconnect();
 app.get("/",(req,res)=>{
 res.send("API IS RUNNING")
 });
+app.use(cors({ credentials:true, origin:process.env.URL}));
 const port=process.env.PORT || 5000
 app.listen(port,console.log(`server started in port ${port}`));
