96c0b1e2e635c7abd01fa8bbd64d8c00ae6a47c2
thamerh <hamdithamer99@gmail.com>
2022-09-18 18:04:07 +0100

    feat: integrate chat feature in express routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,13 @@
 import express from "express";
-import {Register,Login,upload} from "../controllers/user.js"
+import {Register,Login,upload,SearchUsers} from "../controllers/user.js";
+import Protect from "../middleware/Authorization.js";
+import { CreateChat,GetChats,createGroupChat} from "../controllers/Chat.js";
 const router=express.Router()
-
+// user routes
 router.post('/Login',Login)
 router.post('/Register',upload,Register)
+router.route("/user").get(Protect,SearchUsers);
+// chat routes
+router.route("/Chat").post(Protect,CreateChat).get(Protect,GetChats);
+router.post('/chat/group',Protect,createGroupChat)
 export default router;
