53bd0ec3f318c47ec3a6192270860c7cfb6edbc6
thamerh <hamdithamer99@gmail.com>
2022-09-18 23:02:12 +0100

    feat: add enameGroup,removeFromGroup and addToGroup features in express router

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {Register,Login,upload,SearchUsers} from "../controllers/user.js";
 import Protect from "../middleware/Authorization.js";
-import { CreateChat,GetChats,createGroupChat} from "../controllers/Chat.js";
+import { CreateChat,GetChats,createGroupChat,renameGroup,removeFromGroup,addToGroup} from "../controllers/Chat.js";
 const router=express.Router()
 // user routes
 router.post('/Login',Login)
@@ -9,5 +9,8 @@ router.post('/Register',upload,Register)
 router.route("/user").get(Protect,SearchUsers);
 // chat routes
 router.route("/Chat").post(Protect,CreateChat).get(Protect,GetChats);
-router.post('/chat/group',Protect,createGroupChat)
+router.post('/chat/group',Protect,createGroupChat);
+router.put("/chat/rename",Protect,renameGroup);
+router.put("/chat/groupadd",Protect,addToGroup);
+router.put("/chat/groupremove",Protect,removeFromGroup);
 export default router;
