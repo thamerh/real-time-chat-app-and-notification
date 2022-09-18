a0b3e5bf797bc6616a74761dc5f0d8e4a0bbca71
thamerh <hamdithamer99@gmail.com>
2022-09-18 18:01:22 +0100

    fix: fixed user ref in ChatModel

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,26 +1,17 @@
 import mongoose from "mongoose";
 
-const chatModel= mongoose.Schema({
-    chatName:{ type: String ,trim:true},
-    isGroupeChat:{ type:Boolean ,default:false},
-    user:[
-        {
-          type:  mongoose.Schema.Types.ObjectId,
-          ref: "user",
-
-        },
-    ],
-    latesMessages:{
-        type:  mongoose.Schema.Types.ObjectId,
-        ref:"Message",
+const chatModel = mongoose.Schema(
+    {
+      chatName: { type: String, trim: true },
+      isGroupChat: { type: Boolean, default: false },
+      users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      latestMessage: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+      },
+      groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     },
-    groupAdmin:{
-        type:  mongoose.Schema.Types.ObjectId,
-        ref:"user",
-    }
-},
-{
-    timestamps: true,
-});
+    { timestamps: true }
+  );
 const chat= mongoose.model("chat",chatModel);
 export default chat;
