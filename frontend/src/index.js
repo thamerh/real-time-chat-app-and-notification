39333dfb3d0e1858d4136b4c3b7ce85d95d6c048
thamerh <hamdithamer99@gmail.com>
2022-12-23 21:15:13 +0100

    feat: set up the ChakraProvider at the root in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App';
 import ChatProvider from './context/ChatProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChatProvider>
+  <ChakraProvider>
+    <ChatProvider>
         <App />
-  </ChatProvider>
+    </ChatProvider>
+  </ChakraProvider>
 );
 
 
